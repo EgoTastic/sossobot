db27eb56abd25e8cec8cbfbb80d82794fc10711c
Miika Koskela <miika.koskela@hotmail.com>
2022-04-15 21:03:50 +0300

    overlord

--- telegram/index.js
+++ telegram/index.js
@@ -2,12 +2,31 @@ require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const {getQuote, loadQuotes} = require("../sossoquotes/index");
+const overlord = false;
+const overlordId = process.env.OVERLORDID;
 
 bot.command('reloadQuotes', ctx => {
+    if(overlord){
+        return;
+    }
     loadQuotes();
 })
 
+bot.command('overlord_stop', ctx => {
+    if (ctx.from.id == overlordId) {
+        overlord = true;
+    }
+})
+bot.command('overlord_start', ctx => {
+    if (ctx.from.id == overlordId) {
+        overlord = false;
+    }
+})
+
 bot.command("sammakko", ctx => {
+    if(overlord){
+        return;
+    }
     var quote = getQuote();
     bot.telegram.sendMessage(ctx.chat.id, quote);
 })
