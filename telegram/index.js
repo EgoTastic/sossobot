b6eba22974670c41caece602455b46877ce9d88b
Miika Koskela <miika.koskela@hotmail.com>
2022-04-15 21:07:12 +0300

    fixi overlordiin

--- telegram/index.js
+++ telegram/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const {getQuote, loadQuotes} = require("../sossoquotes/index");
-const overlord = false;
+var overlord = false;
 const overlordId = process.env.OVERLORDID;
 
 bot.command('reloadQuotes', ctx => {
