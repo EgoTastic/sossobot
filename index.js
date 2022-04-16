35fdc4f0f716cf6e15751d52c6dec4eca9dbb630
Miika Koskela <miika.koskela@hotmail.com>
2022-04-16 17:23:56 +0300

    launch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { loadQuotes } = require("./sossoquotes");
 const { startTelegramBot } = require("./telegram/index");
 const {connectToDatabase} = require("./database/index");
 
-//Initial launch
+//Initial launch #sorry
 const start = async () => {
 
   await connectToDatabase();
